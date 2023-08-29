3e4803fc572bdea03c85a27ddc95782f2fb2cb4e
fabricio jacob <fabrluc@outlook.com>
2023-08-28 22:52:02 -0300

    add

--- src/main/java/com/github/patterns/concurrencystructure/controller/OrderPersonController.java
+++ src/main/java/com/github/patterns/concurrencystructure/controller/OrderPersonController.java
@@ -3,7 +3,7 @@ package com.github.patterns.concurrencystructure.controller;
 import com.github.patterns.concurrencystructure.dto.OrderPerson;
 import com.github.patterns.concurrencystructure.service.OrderService;
 import com.github.patterns.concurrencystructure.service.PersonService;
-import jdk.incubator.concurrent.StructuredTaskScope;
+//import jdk.incubator.concurrent.StructuredTaskScope;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ExecutionException;
 
+/*
 @RequestMapping("/api/v1/orders")
 @RestController
 public class OrderPersonController {
@@ -34,4 +35,4 @@ public class OrderPersonController {
             return new OrderPerson(user.resultNow(), order.resultNow());
         }
     }
-}
+}*/
